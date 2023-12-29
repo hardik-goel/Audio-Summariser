05709f10cfd9d865293092ff640b3d74c9664fc7
Hardik Goel <hardikgoel@Hardiks-MacBook-Pro.local>
2023-12-29 17:59:58 +0530

    config file push

---
+++ config/conf.py
@@ -0,0 +1,12 @@
+AUD_FILE_PATH = "/Users/hardikgoel/Downloads/trials/sentence_summary/sample_order_recording.wav"
+# AUD_FILE_PATH = "resources/sample_order_recording.wav"
+
+#ONlY TO BE USED IF NOT RUNNING THE ENTIRE THING
+
+# FILE_PATH = "/Users/hardikgoel/Downloads/trials/sentence_summary/new/sample_text_orders.txt"
+# FILE_PATH = "resources/sample_text_limitation.txt"
+# FILE_PATH = "/Users/hardikgoel/Downloads/trials/sentence_summary/new/sample_text_limitation.txt"
+# FILE_PATH = "resources/sample_text_orders.txt"
+
+MODEL_NAME = "facebook/bart-large-cnn"
+
