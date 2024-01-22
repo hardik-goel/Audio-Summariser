fffe1ccaa3e824c8060ea8c701b893b2600ff973
Hardik Goel <hardikgoel@Hardiks-MacBook-Pro.local>
2024-01-22 21:57:55 +0530

    cosmetic changes

--- config/conf.py
+++ config/conf.py
@@ -1,13 +1,4 @@
-# AUD_FILE_PATH = "/Users/hardikgoel/Downloads/trials/sentence_summary/sample_order_recording.wav"
-# AUD_FILE_PATH = "resources/sample_order_recording.wav"
-# AUD_FILE_PATH = "/Users/hardikgoel/Downloads/github/AudioSummariser/resources/Customer_Service_Product_Refund.wav"
-AUD_FILE_PATH = "/Users/hardikgoel/Downloads/github/AudioSummariser/data/"
-#ONlY TO BE USED IF NOT RUNNING THE ENTIRE THING
-
-# FILE_PATH = "/Users/hardikgoel/Downloads/trials/sentence_summary/new/sample_text_orders.txt"
-# FILE_PATH = "resources/sample_text_limitation.txt"
-# FILE_PATH = "/Users/hardikgoel/Downloads/trials/sentence_summary/new/sample_text_limitation.txt"
-# FILE_PATH = "resources/sample_text_orders.txt"
+AUD_FILE_PATH = "data/"
 MODEL_NAME = "gemini-pro"
 API_KEY = "<YOUR-API-KEY>"
 
--- utilities/file_reads.py
+++ utilities/file_reads.py
@@ -1,11 +1,12 @@
 import glob
 import os
+
 from config.conf import AUD_FILE_PATH
 
 file_paths = glob.glob(os.path.join(AUD_FILE_PATH, '*'))
 
+
 def fetch_audio_files():
     for file_path in file_paths:
-        filename = os.path.basename(file_path)
-        print(f"Processing file: {filename}")
+        print(f"Processing file: {file_path}")
     return file_path
